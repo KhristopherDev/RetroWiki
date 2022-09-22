7de2f219eb6c5e5850d576373ccb66afa86bfaf3
KhristopherDev <86271495+KhristopherDev@users.noreply.github.com>
2022-09-21 21:43:07 -0300

    Add files via upload

---
+++ project_retro/js/script.js
@@ -0,0 +1,17 @@
+const logo = document.getElementsByClassName('logo');
+const logotitle = document.getElementsByClassName('logotitle');
+const logoimage = document.getElementById('logoimage');
+
+const winw = window.innerWidth;
+const winh = window.innerHeight;
+
+//console.log(winw);
+
+
+//const verif = () => {
+//    if (winw < 1000) {
+//        logotitle.style.fontSize = '1000px'
+//    } else {
+//        logotitle.style.flexDirection = 'column'
+//    }
+//    }
